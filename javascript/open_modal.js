bef523c7c3998a32c6d97699ea1dcc273ecffdd2
Hatterrine <perrine.goget@yahoo.com>
2025-06-07 12:14:35 +0200

    arrangement de la gestion des css dans dessin et photo

--- javascript/open_modal.js
+++ javascript/open_modal.js
@@ -2,9 +2,11 @@
         const modal = document.querySelector(".modal");
         const modalImage = document.querySelector(".modal-image");
         modalImage.src = element.querySelector('img').src;
-        modal.style.display = "flex";   }
+        modal.style.display = "flex";   
+        document.querySelector('.banner-construction').style.display = 'none';}
 
     function closeModal() {
         const modal = document.querySelector(".modal");
         modal.style.display = "none";
+        document.querySelector('.banner-construction').style.display = '';
     }
